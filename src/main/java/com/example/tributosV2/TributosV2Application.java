55549ca359af037b2bd637e3066147f05cb9848d
Mateuslh <91639503+Mateuslh@users.noreply.github.com>
2024-04-10 00:17:54 -0300

    Init

---
+++ src/main/java/com/example/tributosV2/TributosV2Application.java
@@ -0,0 +1,12 @@
+package com.example.tributosV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TributosV2Application {
+	public static void main(String[] args) {
+		SpringApplication.run(TributosV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tributosV2/TributosV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tributosV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TributosV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
