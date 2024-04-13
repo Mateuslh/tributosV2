af55abb378a1fa386e27acc83a2f64581574252a
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-04-13 12:19:05 -0300

    [feature] - implementado validação de cpf na classe PessoaFisica.

--- src/main/java/com/example/tributosV2/model/Contribuinte/PessoaFisica.java
+++ src/main/java/com/example/tributosV2/model/Contribuinte/PessoaFisica.java
@@ -1,10 +1,13 @@
 package com.example.tributosV2.model.Contribuinte;
 
+import com.example.tributosV2.exception.ValidationException;
+import com.example.tributosV2.util.ValidadorDocumento;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+
 @Getter
 @Setter
 @Entity(name = "PessoaFisica")
@@ -16,6 +19,13 @@ public class PessoaFisica extends Contribuinte {
 
     @Column(name = "rg", nullable = false,unique = true)
     private String rg;
+
+    public void setCpf(String cpf) {
+        if (!ValidadorDocumento.validarCPF(cpf)){
+            throw new ValidationException("Cpf não é valido para inserção!");
+        };
+        this.rg = cpf;
+    }
 
     @Override
     public String getDocumentoPrincipal() {
