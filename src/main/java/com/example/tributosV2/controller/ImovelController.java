5404a1ac2c1ff6ad706bc26fb3f4f7f7f959f737
Mateuslh <91639503+Mateuslh@users.noreply.github.com>
2024-04-22 23:56:34 -0300

    [fix] - Inserido o método de busca de debitos por imovel.

--- src/main/java/com/example/tributosV2/controller/ImovelController.java
+++ src/main/java/com/example/tributosV2/controller/ImovelController.java
@@ -1,9 +1,13 @@
 package com.example.tributosV2.controller;
 
 
+import com.example.tributosV2.exception.NotFoundException;
 import com.example.tributosV2.model.imovel.Imovel;
 import com.example.tributosV2.service.ImovelService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,10 @@ public class ImovelController extends AbstractCadastralController<Imovel, Imovel
     @Autowired
     public ImovelController(ImovelService service) {
         super(service, Imovel.class);
+    }
+
+    @GetMapping("/{id}/getDebitos")
+    public ResponseEntity<?> getDebitos(@PathVariable Long id) throws NotFoundException {
+        return ResponseEntity.ok().body(service.getDebitos(id));
     }
 }
--- src/main/java/com/example/tributosV2/service/ImovelService.java
+++ src/main/java/com/example/tributosV2/service/ImovelService.java
@@ -1,10 +1,21 @@
 package com.example.tributosV2.service;
 
+import com.example.tributosV2.exception.NotFoundException;
+import com.example.tributosV2.model.Debito.Debito;
 import com.example.tributosV2.model.imovel.Imovel;
+import com.example.tributosV2.repository.DebitoRepository;
 import com.example.tributosV2.repository.ImovelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ImovelService extends AbstractCadastralService<Imovel, ImovelRepository> {
+    @Autowired
+    protected DebitoRepository debitoRepository;
 
+    public List<Debito> getDebitos(Long imovelId) throws NotFoundException {
+        return debitoRepository.findDebitoByImovelId(imovelId);
+    }
 }
